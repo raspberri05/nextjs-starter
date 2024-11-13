a6dc91e7278a117849d094519998092e026b727c
Naya Singhania <naya.singhania@gmail.com>
2024-11-13 13:30:43 -0800

    add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 export default function Page() {
-  return (
-    <div>
-      <h1>Next.js + Tailwind + Shadcn + Prettier Starter Template</h1>
-      <br />
-      <p>Shadcn is used for UI</p>
-      <br />
-      <p>This started also had a dark mode toggle built in</p>
-      <br />
-      <p>This page is just an example page without a header or footer. I use this template as a starter for most of my projects</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Next.js + Tailwind + Shadcn + Prettier Starter Template</h1>
+            <br />
+            <p>Shadcn is used for UI</p>
+            <br />
+            <p>This started also had a dark mode toggle built in</p>
+            <br />
+            <p>
+                This page is just an example page without a header or footer. I
+                use this template as a starter for most of my projects
+            </p>
+        </div>
+    );
 }
